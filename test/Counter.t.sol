11580606edb013de33018793e3d267dad59e0a84
sudotx <64108848+sudotx@users.noreply.github.com>
2024-02-13 00:55:01 +0100

    cle

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {FrameGame, FrameToken} from "../src/Counter.sol";
+import {FrameGame, FrameToken} from "src/3423.sol";
 
 contract CounterTest is Test {
     FrameGame game;
